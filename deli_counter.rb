9b66b5676e8b054aefd185381852f52d74f6f4f4
Eryk Rachwal <erykrachwal@yahoo.com>
2019-12-18 18:00:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+require "pry"
+def line(katz_deli)
+  if katz_deli.empty?
+  puts "The line is currently empty." 
+  else 
+    current_line = "The line is currently:"
+      katz_deli.each.with_index do |val,index|
+      current_line += " #{index + 1}. #{val}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  if katz_deli.empty?
+    puts "Welcome, #{name}. You are number 1 in line."
+  else 
+    puts  "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  end
+  katz_deli
+end
+
